59f4584b3606752132a003b7485ecdde8b9697c5
Merzaad <Merzaad@gmail.com>
2022-06-07 20:54:59 +0430

    Add timer ( using Copilot AI )

--- src/pages/popup/App.tsx
+++ src/pages/popup/App.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
 
 const App = (): JSX.Element => {
+  const [time, setTime] = React.useState('')
+
+  // a function that returns time in a format of hh:mm:ss
+  // using Copilot AI
+
+  const getTime = (): string => {
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+    return `${hours}:${minutes}:${seconds}`
+  }
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(getTime())
+    }, 1000)
+    return () => clearInterval(interval)
+  })
   return (
-    <div>
-      <h1>Popup Page</h1>
-      <p>If you are seeing this, React is working!</p>
+    <div
+      style={{
+        width: '200px',
+        padding: '0px',
+      }}
+    >
+      <div
+        style={{
+          background: 'gray',
+          display: 'flex',
+          borderRadius: '10px',
+          width: '100%',
+          justifyContent: 'center',
+          color: 'white',
+          fontSize: '30px',
+        }}
+      >
+        <h6 style={{ textShadow: '0px 0px 3px white' }}>{time}</h6>
+      </div>
     </div>
   )
 }
