7c66985c7941905e911c853fb18500415ee355c2
Merzaad <Merzaad@gmail.com>
2022-06-15 15:16:50 +0430

    Add react query devtools

--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import { QueryClientProvider, QueryClient } from 'react-query'
 import PopUp from './PopUp'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
 const root = document.querySelector('#root')
 const queryClient = new QueryClient()
@@ -9,6 +10,7 @@ const queryClient = new QueryClient()
 render(
   <QueryClientProvider client={queryClient}>
       <PopUp />
+      <ReactQueryDevtools position='bottom-right' />
   </QueryClientProvider>,
   root,
 )
