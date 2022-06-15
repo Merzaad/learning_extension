6016f3025198506dd77f9432aa81c84c1511f786
Merzaad <Merzaad@gmail.com>
2022-06-15 15:21:54 +0430

    Add scale in hover action

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,24 +11,27 @@ const Card = ({ symbol, id = symbol }: prop): JSX.Element => {
   const lIGTH_BACKGROUND = '#222831'
   const DARK_COLOR = '#FFD369'
   const DARK_BACKGROUND = '#393E46'
-  
 
   const [color, setColor] = React.useState(lIGTH_COLOR)
   const [backgroundColor, setBackgroundColor] = React.useState(lIGTH_BACKGROUND)
+  const [scale, setScale] = React.useState(1)
   const hover = (action: string): void => {
     if (action === 'in') {
       setColor(DARK_COLOR)
       setBackgroundColor(DARK_BACKGROUND)
+      setScale(1.02)
     }
     if (action === 'out') {
       setColor(lIGTH_COLOR)
       setBackgroundColor(lIGTH_BACKGROUND)
+      setScale(1)
     }
   }
   return (
     <div
       key={id}
       style={{
+        transform: `scale(${scale})`,
         width: 300,
         background: backgroundColor,
         display: 'flex',
@@ -49,7 +52,7 @@ const Card = ({ symbol, id = symbol }: prop): JSX.Element => {
             textDecoration: 'none',
             transitionDuration: '0.3s',
             margin: 10,
-            fontWeight: 200,
+            fontWeight: 300,
             fontSize: 15,
             display: 'flex',
             gap: 20,
@@ -57,32 +60,26 @@ const Card = ({ symbol, id = symbol }: prop): JSX.Element => {
         >
           <div
             style={{
-              color: "#EEEEEE",
+              color: '#EEEEEE',
               display: 'flex',
               flexDirection: 'column',
               gap: 10,
-              width: 130
+              width: 130,
             }}
           >
-            <span style={{ opacity: 0.5 }}>
-              {'Price : '}
-              </span>
-            <span style={{ opacity: 0.5 }}>
-              {'Transactions 24h: '}
-            </span>
-            <span style={{ opacity: 0.5 }}>
-              {'Change 24h: '}
-            </span>
+            <span style={{ opacity: 0.5 }}>{'Price : '}</span>
+            <span style={{ opacity: 0.5 }}>{'Transactions 24h: '}</span>
+            <span style={{ opacity: 0.5 }}>{'Change 24h: '}</span>
           </div>
           <div
-          style={{
-            display: 'flex',
-            color: color,
-            flexDirection: 'column',
-            textShadow: `0px 0px 5px ${color}`,
-            gap: 10,
-            width: 70
-          }}
+            style={{
+              display: 'flex',
+              color: color,
+              flexDirection: 'column',
+              textShadow: `0px 0px 2px ${color}`,
+              gap: 10,
+              width: 70,
+            }}
           >
             <span>
               {query.data?.data.market_price_usd}
@@ -94,9 +91,9 @@ const Card = ({ symbol, id = symbol }: prop): JSX.Element => {
               {query.isLoading && 'loading'}
               {query.error && 'error'}
             </span>
-            <span
-            >
-              {query.data?.data.market_price_usd_change_24h_percentage}{'%'}
+            <span>
+              {query.data?.data.market_price_usd_change_24h_percentage}
+              {'%'}
               {query.isLoading && 'loading'}
               {query.error && 'error'}
             </span>
